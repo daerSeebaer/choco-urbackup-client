3317f65df4419b4e87718dcba718837e088ed982
daerSeebaer <61566539+daerSeebaer@users.noreply.github.com>
2023-05-15 13:22:09 +0200

    update to 2.5.24

--- pkgfiles/tools/chocolateyinstall.ps1
+++ pkgfiles/tools/chocolateyinstall.ps1
@@ -5,18 +5,19 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = ''
 
 $pp = Get-PackageParameters
+# default to False
 if (!$pp.TrayIcon){ 
-	$pp.TrayIcon = 'False' 
-	$UrExe = 'https://hndl.urbackup.org/Client/2.5.23/UrBackup%20Client%20NoTray%202.5.23.exe'
-	$ChkSum = '814B39E44A1C31A84B09756AD121487C4453AA3D4CE49DCABCBE49113784C426'
+	$pp.TrayIcon = 'False'
+  $UrExe = 'https://hndl.urbackup.org/Client/2.5.24/UrBackup%20Client%20NoTray%202.5.24.exe'
+	$ChkSum = '20A450644392245F456D0A22A73D460E17D8EE7C4DFD1DAD8D084BD6F58525CB'
 }
 if ($pp.TrayIcon -eq 'False'){
-	$UrExe = 'https://hndl.urbackup.org/Client/2.5.23/UrBackup%20Client%20NoTray%202.5.23.exe'
-	$ChkSum = '814B39E44A1C31A84B09756AD121487C4453AA3D4CE49DCABCBE49113784C426'
+	$UrExe = 'https://hndl.urbackup.org/Client/2.5.24/UrBackup%20Client%20NoTray%202.5.24.exe'
+	$ChkSum = '20A450644392245F456D0A22A73D460E17D8EE7C4DFD1DAD8D084BD6F58525CB'
 }
 if ($pp.TrayIcon -eq 'True'){
-	$UrExe = 'https://hndl.urbackup.org/Client/2.5.23/UrBackup%20Client%202.5.23.exe'
-	$ChkSum = 'B1B3B859981708E1E10DC9AAA1DD179533CF0F8BCD08DC2B4BC5D71CC549B07D'
+	$UrExe = 'https://hndl.urbackup.org/Client/2.5.24/UrBackup%20Client%202.5.24.exe'
+	$ChkSum = '58E64B68B807BF467548A79774D01E50A42F7F712B3278C92C0EBDFC93A1A8C8'
 }
 
 #Based on Nsis
